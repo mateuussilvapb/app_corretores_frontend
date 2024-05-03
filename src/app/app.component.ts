22eb72cc417c172c2cfa6e6d179802a6b9718329
Mateus Dias <mateuussilvapb@gmail.com>
2024-05-02 21:27:43 -0300

    Feature: Configuração do PrimeNG

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+//Angular
+import { Component, OnInit } from '@angular/core';
+
+//Externos
+import { PrimeNGConfig } from 'primeng/api';
+
+//Internos
+import { Opcoes } from './config/primeNG/traducao.config';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'app_corretores_frontend';
+export class AppComponent implements OnInit {
+  constructor(private primengConfig: PrimeNGConfig) {}
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+    this.primengConfig.setTranslation(Opcoes.traducaoPtBr);
+  }
 }
