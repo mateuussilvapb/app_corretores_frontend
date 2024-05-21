e76867a7fa2b13c8ab5e9b94527eb603146a53ae
Mateus Dias <mateuussilvapb@gmail.com>
2024-05-21 09:27:11 -0300

    Feature: Ajuste no componente de loading

--- src/app/shared/components/loading/loading.component.ts
+++ src/app/shared/components/loading/loading.component.ts
@@ -1,13 +1,19 @@
 //Angular
 import { Component } from '@angular/core';
 
-//Externos
-import { ProgressSpinnerModule } from 'primeng/progressspinner';
-
 @Component({
   selector: 'app-loading',
   standalone: true,
-  imports: [ProgressSpinnerModule],
-  template: ` <p-progressSpinner ariaLabel="loading" /> `,
+  imports: [],
+  template: `
+    <div class="loader">
+      <div class="dot dot-1"></div>
+      <div class="dot dot-2"></div>
+      <div class="dot dot-3"></div>
+      <div class="dot dot-4"></div>
+      <div class="dot dot-5"></div>
+    </div>
+  `,
+  styleUrls: ['./loading.component.scss'],
 })
 export class LoadingComponent {}
