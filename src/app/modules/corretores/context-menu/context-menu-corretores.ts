276df2937f9db5df5f1a9a50b21e3f45641fb76e
Mateus Dias <mateuussilvapb@gmail.com>
2024-05-31 15:00:26 -0300

    Feature: adicionando opção de navegação para visualizar veículos atribuidos a corretores

--- src/app/modules/corretores/context-menu/context-menu-corretores.ts
+++ src/app/modules/corretores/context-menu/context-menu-corretores.ts
@@ -2,23 +2,25 @@
 import { Subject } from 'rxjs';
 
 //Externos
-import { Menu } from "primeng/menu";
+import { Menu } from 'primeng/menu';
 import { MessageService } from 'primeng/api';
 import { ConfirmationService } from 'primeng/api';
 import { KeycloakService } from 'keycloak-angular';
 
 //Internos
 import { ROLES } from 'src/app/shared/model/roles';
-import { Corretor } from "src/app/shared/model/corretor";
+import { Corretor } from 'src/app/shared/model/corretor';
 import { CorretoresService } from 'src/app/modules/corretores/services/corretores.service';
-import { ContextMenu, MenuItensWithPermissions } from "src/app/shared/context-menu/context-menu";
+import {
+  ContextMenu,
+  MenuItensWithPermissions,
+} from 'src/app/shared/context-menu/context-menu';
 
 export interface ContextMenuCorretoresData {
   corretor: Corretor;
 }
 
 export class ContextMenuCorretores extends ContextMenu<ContextMenuCorretoresData> {
-
   constructor(
     actionMenu: Menu,
     keycloakService: KeycloakService,
@@ -30,39 +32,36 @@ export class ContextMenuCorretores extends ContextMenu<ContextMenuCorretoresData
     super(actionMenu, keycloakService);
   }
 
-  protected override menuItems = (data: ContextMenuCorretoresData): MenuItensWithPermissions[] => [
+  protected override menuItems = (
+    data: ContextMenuCorretoresData
+  ): MenuItensWithPermissions[] => [
     {
       id: 'visualizar',
       label: 'Visualizar',
       icon: 'pi pi-search',
       url: `${this.baseHref}/corretores/${data.corretor.id}/visualizar`,
-      rolesAllowed: [
-        ROLES.ADMIN,
-        ROLES.MANAGER,
-        ROLES.EMPLOYEE,
-      ]
+      rolesAllowed: [ROLES.ADMIN, ROLES.MANAGER, ROLES.EMPLOYEE],
     },
     {
       id: 'editar',
       label: 'Editar',
       icon: 'pi pi-user-edit',
       url: `${this.baseHref}/corretores/${data.corretor.id}/editar`,
-      rolesAllowed: [
-        ROLES.ADMIN,
-        ROLES.MANAGER,
-        ROLES.EMPLOYEE,
-      ]
+      rolesAllowed: [ROLES.ADMIN, ROLES.MANAGER, ROLES.EMPLOYEE],
     },
     {
       id: 'vales',
       label: 'Vales',
       icon: 'pi pi-dollar',
       url: `${this.baseHref}/corretores/${data.corretor.id}/vales`,
-      rolesAllowed: [
-        ROLES.ADMIN,
-        ROLES.MANAGER,
-        ROLES.EMPLOYEE,
-      ]
+      rolesAllowed: [ROLES.ADMIN, ROLES.MANAGER, ROLES.EMPLOYEE],
+    },
+    {
+      id: 'veiculos',
+      label: 'Veículos',
+      icon: 'pi pi-car',
+      url: `${this.baseHref}/corretores/${data.corretor.id}/veiculos`,
+      rolesAllowed: [ROLES.ADMIN, ROLES.MANAGER, ROLES.EMPLOYEE],
     },
     {
       id: 'excluir',
@@ -71,17 +70,14 @@ export class ContextMenuCorretores extends ContextMenu<ContextMenuCorretoresData
       command: () => {
         this.onExcluir(data.corretor.idString);
       },
-      rolesAllowed: [
-        ROLES.ADMIN,
-        ROLES.MANAGER,
-        ROLES.EMPLOYEE,
-      ]
-    }
-  ]
+      rolesAllowed: [ROLES.ADMIN, ROLES.MANAGER, ROLES.EMPLOYEE],
+    },
+  ];
 
   private onExcluir(idString: string) {
     this.confirmationService.confirm({
-      message: 'Tem certeza que deseja excluir este corretor? A ação não poderá ser desfeita.',
+      message:
+        'Tem certeza que deseja excluir este corretor? A ação não poderá ser desfeita.',
       header: 'Confirma?',
       icon: 'pi pi-exclamation-triangle',
       acceptButtonStyleClass: 'p-button-danger',
@@ -96,7 +92,7 @@ export class ContextMenuCorretores extends ContextMenu<ContextMenuCorretoresData
         summary: 'Sucesso',
         detail: 'Corretor excluído com sucesso.',
         life: 5000,
-      })
+      });
       this.refresh$.next();
     });
   }
